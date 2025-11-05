1680abf8883275dc1c43171a7cbf6a2853ad66d1
amiqdam <amiqdam@gmail.com>
2025-11-05 14:00:11 +0700

    Update db_connection.py
    Connect to MySQL without revealing data

--- db_connection.py
+++ db_connection.py
@@ -1,1 +1,22 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
 
+# Load variabel dari file .env
+load_dotenv()
+
+def connect_db():
+    """
+    Membuat koneksi ke database MySQL menggunakan konfigurasi dari .env
+    """
+    try:
+        db = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        return db
+    except mysql.connector.Error as e:
+        print(f"❌ Gagal konek ke database: {e}")
+        return None
